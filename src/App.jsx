1b28c7b23f0989435815217b3f26807c7d19d7f5
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-25 18:32:57 +0200

    resume speeddial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { Box } from '@mui/material'
+import { Box, SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material'
+import PrintIcon from '@mui/icons-material/Print';
 import './App.css'
 import styled from '@emotion/styled'
 import {theme} from './theme'
 import { About, Contact, Footer, Hero, Projects, Toolbox } from './Components'
+import resume from '../public/resume.pdf'
 
 
 const MainContainer = styled(Box)({
@@ -10,11 +12,36 @@ const MainContainer = styled(Box)({
   width: '100%'
 })
 
+const speedStyle = {
+  position: 'absolute', 
+  bottom: 20, 
+  right: {
+    xs: 20,
+    sm: 20,
+    md: 100,
+    lg: 320
+  }, 
+  color: theme.palette.primary.dark}
+
 function App() {
 
   return (
     <>
       <MainContainer>
+        <div>
+          <SpeedDial
+            ariaLabel='speed dial'
+            sx={speedStyle}
+            icon={<SpeedDialIcon />}
+          >
+            <SpeedDialAction
+              icon={<PrintIcon />}
+              tooltipTitle={"Resume"}
+              tooltipOpen
+              onClick={() => window.open(resume)}
+            />
+          </SpeedDial>
+        </div>
         <Hero />
         <About />
         <Toolbox />
